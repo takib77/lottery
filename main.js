c42944e552c5c04b04060f32e1b81ca74382c9f2
Leo <takib77@gmail.com>
2021-02-12 17:38:34 +0100

    Write result in DOM

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Items selectors //
+// Items selectors
 
 const domItems = {
     setName: document.querySelector('#userName'),
@@ -20,33 +20,38 @@ const domItems = {
     lotteryNumber2: document.querySelector('#lotteryTableNum2'),
     lotteryNumber3: document.querySelector('#lotteryTableNum3'),
     lotteryNumber4: document.querySelector('#lotteryTableNum4'),
-    lotteryNumber5: document.querySelector('#lotteryTableNum5')
+    lotteryNumber5: document.querySelector('#lotteryTableNum5'),
+    lotteryResult: document.querySelector('#result')
 }
 
-let userNumbers;
+// Get userdatas
+
+let userNumbers = [];
 const onStartClick = () => {
     domItems.start.addEventListener('click', () => {
-        userNumbers = [
+        userNumbers.push(
             parseInt(domItems.setNumber1.value),
             parseInt(domItems.setNumber2.value),
             parseInt(domItems.setNumber3.value),
             parseInt(domItems.setNumber4.value),
             parseInt(domItems.setNumber5.value)
-        ];
+        );
         domItems.writeName.textContent = domItems.setName.value;
         domItems.writeNumber1.textContent = userNumbers[0];
         domItems.writeNumber2.textContent = userNumbers[1];
         domItems.writeNumber3.textContent = userNumbers[2];
         domItems.writeNumber4.textContent = userNumbers[3];
         domItems.writeNumber5.textContent = userNumbers[4];
-        lotteryEvent();
+        lotteryDrawing();
     });
 }
+
+// Delay with async
 
 const delay = (delayInSecond) => {
     return new Promise(resolve => {
         setTimeout(() => {
-            resolve(userNumbers);
+            resolve();
         }, delayInSecond * 1000);
     })
 }
@@ -55,9 +60,10 @@ async function delayCall() {
     const result = await delay(2);
 }
 
+// Drawing method
 
-async function lotteryEvent(drawNumber = 5, possibleNumber = 90) {
-    let lotteryNumbers = [];
+let lotteryNumbers = [];
+async function lotteryDrawing(drawNumber = 5, possibleNumber = 90) {
     let randomNumber;
     for (let i = 1; i <= drawNumber; i++) {
         randomNumber = Math.floor(Math.random() * (possibleNumber - 1)) + 1;
@@ -73,6 +79,23 @@ async function lotteryEvent(drawNumber = 5, possibleNumber = 90) {
     domItems.lotteryNumber4.textContent = lotteryNumbers[3];
     await delayCall();
     domItems.lotteryNumber5.textContent = lotteryNumbers[4];
+    compareNums();
+}
+
+// Comparison of numbers
+
+let counter = 0;
+const compareNums = () => {
+    for (let i = 0; i < userNumbers.length; i++) {
+        if (lotteryNumbers.includes(userNumbers[i])) {
+            counter += 1;
+        }
+    }
+    if (counter <= 1) {
+        domItems.lotteryResult.textContent = (`Sorry ${domItems.setName.value}, you have ${counter} hit in the Lottery! Try again next week!`);
+    } else if (counter > 1) {
+        domItems.lotteryResult.textContent = (`Congratulation ${domItems.setName.value}, you have ${counter} hit in the Lottery! You are lucky!`);
+    }
 }
 
 
